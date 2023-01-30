28a11782a5966a47c7721c03a8a7d0d39352c681
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-01-30 21:58:59 +0200

    fix(): finish the calculate form

--- src/components/form/DeliveryFeeCalculatorForm.tsx
+++ src/components/form/DeliveryFeeCalculatorForm.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { format } from 'date-fns';
-import { Form, Formik, FormikErrors, FormikTouched } from 'formik';
-import { StyledForm } from './DeliveryFeeCalculatorForm.styles';
+import React, { useState } from 'react';
+import { Form, Formik } from 'formik';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { Delivery } from '../../types';
-
-type Props = {};
 
 interface Errors {
   cartValue?: string;
@@ -21,7 +17,9 @@ const initialValues: Delivery = {
   time: '',
 };
 
-const DeliveryFeeCalculatorForm: React.FC<Props> = () => {
+const DeliveryFeeCalculatorForm: React.FC = () => {
+  const [totalDeliveryFee, setTotalDeliveryFee] = useState<number>(0);
+
   const validate = (values: Delivery) => {
     const errors: Errors = {};
 
@@ -33,6 +31,8 @@ const DeliveryFeeCalculatorForm: React.FC<Props> = () => {
 
     if (!values.deliveryDistance && values.deliveryDistance !== 0) {
       errors.deliveryDistance = 'required';
+    } else if (!/^\d+$/.test(values.deliveryDistance.toString())) {
+      errors.deliveryDistance = 'Must be an integer';
     } else if (values.deliveryDistance < 0) {
       errors.deliveryDistance = 'The distance cannot not be negative';
     }
@@ -41,13 +41,46 @@ const DeliveryFeeCalculatorForm: React.FC<Props> = () => {
       errors.numberOfItems = 'required';
     } else if (!/^\d+$/.test(values.numberOfItems.toString())) {
       errors.numberOfItems = 'Must be an integer';
+    } else if (values.numberOfItems < 0) {
+      errors.numberOfItems = 'The number of items cannot not be negative';
+    }
+
+    if (!values.time) {
+      errors.time = 'required';
     }
     return errors;
   };
 
+  const calculateDeliveryFee = (values: Delivery) => {
+    if (values.cartValue >= 100) {
+      return 0;
+    }
+    const orderSurcharge = Math.max(10 - values.cartValue, 0);
+    const distanceFee =
+      values.deliveryDistance <= 1000
+        ? 2
+        : 2 + Math.floor((values.deliveryDistance - 501) / 500);
+    let perItemFee = 0;
+    if (values.numberOfItems > 4) {
+      perItemFee = (values.numberOfItems - 4) * 0.5;
+      if (values.numberOfItems > 12) {
+        perItemFee += 1.2;
+      }
+    }
+    const day = new Date(values.time).getDay();
+    const hours = new Date(values.time).getHours();
+    let res = 0;
+    if (day === 5 && hours >= 15 && hours < 19) {
+      res = 1.2 * (orderSurcharge + distanceFee + perItemFee);
+    } else {
+      res = orderSurcharge + distanceFee + perItemFee;
+    }
+    return Math.min(res, 15);
+  };
+
   const handleSubmit = (values: Delivery) => {
-    //s
-  }
+    setTotalDeliveryFee(Math.round(calculateDeliveryFee(values) * 100) / 100);
+  };
 
   return (
     <Formik<Delivery>
@@ -58,7 +91,7 @@ const DeliveryFeeCalculatorForm: React.FC<Props> = () => {
       validateOnBlur={false}
     >
       {({ values, errors, handleChange, handleBlur, handleSubmit }) => (
-        <StyledForm onSubmit={handleSubmit} noValidate>
+        <Form onSubmit={handleSubmit} noValidate>
           <Box
             mt={2}
             display='flex'
@@ -87,7 +120,9 @@ const DeliveryFeeCalculatorForm: React.FC<Props> = () => {
               onChange={handleChange}
               onBlur={handleBlur}
               error={!!errors['deliveryDistance']}
-              helperText={!!errors['deliveryDistance'] && errors['deliveryDistance']}
+              helperText={
+                !!errors['deliveryDistance'] && errors['deliveryDistance']
+              }
               required
             />
             <TextField
@@ -103,34 +138,21 @@ const DeliveryFeeCalculatorForm: React.FC<Props> = () => {
               required
             />
             <TextField
-              label='Time'
               name='time'
-              type='string'
               value={values.time}
+              type='datetime-local'
               onChange={handleChange}
               onBlur={handleBlur}
               error={!!errors['time']}
               helperText={!!errors['time'] && errors['time']}
               required
             />
-            <TextField
-              name='time'
-              value={values.time}
-              type='datetime-local'
-              InputLabelProps={{ shrink: true }}
-              inputProps={{
-                min: format(new Date(), "yyyy-MM-dd'T'HH:mm"),
-              }}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              required
-            />
             <Button type='submit' variant='contained'>
               Submit
             </Button>
-            <Typography>Delivery price: </Typography>
+            <Typography>Delivery price: {totalDeliveryFee}</Typography>
           </Box>
-        </StyledForm>
+        </Form>
       )}
     </Formik>
   );
